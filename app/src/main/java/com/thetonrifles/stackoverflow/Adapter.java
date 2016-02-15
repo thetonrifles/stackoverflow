59487faf86946b2d10f0afc98890de3bebd21945
Andrea Petreri <andrea.petreri@gmail.com>
2016-02-15 09:26:18 +0100

    Adding click listener for button

--- app/src/main/java/com/thetonrifles/stackoverflow/Adapter.java
+++ app/src/main/java/com/thetonrifles/stackoverflow/Adapter.java
@@ -5,22 +5,34 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ListViewHolder> {
 
-    protected static class ListViewHolder extends RecyclerView.ViewHolder {
+    protected class ListViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         ImageView img_photo;
         TextView txt_label;
+        Button btn_pick;
 
         public ListViewHolder(View itemView) {
             super(itemView);
             txt_label = (TextView) itemView.findViewById(R.id.txt_label);
             img_photo = (ImageView) itemView.findViewById(R.id.img_photo);
+            btn_pick = (Button) itemView.findViewById(R.id.btn_pick);
+            btn_pick.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            ListItem item = mItems.get(getAdapterPosition());
+            // do work here
+            Toast.makeText(mContext, item.getLabel(), Toast.LENGTH_SHORT).show();
         }
 
     }
